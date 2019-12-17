8575c857032b2799a08ac88e3281784ba7f7096d
lizzykerrigan <lizzykerrigan@outlook.com>
2019-12-17 20:32:24 +0000

    initial contentful config

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import * as contentful from 'contentful';
 import * as serviceWorker from "./serviceWorker";
+import { REACT_APP_CTF_SPACE, REACT_APP_CTF_CDA_TOKEN } from "./env.js"
+
+const client = contentful.createClient({
+    space: REACT_APP_CTF_SPACE,
+    accessToken: REACT_APP_CTF_CDA_TOKEN })
+    client.getEntries().then(entries => {
+    entries.items.forEach(entry => {
+      if(entry.fields) {
+        console.log(entry.fields)
+      }
+    })
+  })
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
