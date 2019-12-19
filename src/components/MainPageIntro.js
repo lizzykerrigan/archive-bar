aa365062b78b8d07a672574f15aa663ce669b33a
Lizzy Kerrigan <lizzykerrigan@outlook.com>
2019-12-19 20:46:45 +0000

    remove outer div

--- src/components/MainPageIntro.js
+++ src/components/MainPageIntro.js
@@ -1,26 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const MainPageIntro = () => {
-    return (
-        <div className="section-2 cc-store-home-wrap">
-          <div className="intro-head">
-            <img
-              src="https://uploads-ssl.webflow.com/5df20ac39bca42668d1d0fac/5df2a3625526fc59befe5c3c_53169366_249289682676967_8442834466077933568_n%20(1).png"
-              width="480"
-              srcSet="
+  return (
+    <div className="intro-head">
+      <img
+        src="https://uploads-ssl.webflow.com/5df20ac39bca42668d1d0fac/5df2a3625526fc59befe5c3c_53169366_249289682676967_8442834466077933568_n%20(1).png"
+        width="480"
+        srcSet="
           https://uploads-ssl.webflow.com/5df20ac39bca42668d1d0fac/5df2a3625526fc59befe5c3c_53169366_249289682676967_8442834466077933568_n%20(1)-p-500.png 500w,
           https://uploads-ssl.webflow.com/5df20ac39bca42668d1d0fac/5df2a3625526fc59befe5c3c_53169366_249289682676967_8442834466077933568_n%20(1)-p-800.png 800w,
           https://uploads-ssl.webflow.com/5df20ac39bca42668d1d0fac/5df2a3625526fc59befe5c3c_53169366_249289682676967_8442834466077933568_n%20(1).png       960w
         "
-              sizes="100vw"
-              data-w-id="25adf555-9902-15fe-6052-25565d02d245"
-              style={{opacity: 1}}
-              alt=""
-              className="image-3"
-            />
-          </div>
-        </div>
-    )
-}
+        sizes="100vw"
+        data-w-id="25adf555-9902-15fe-6052-25565d02d245"
+        style={{ opacity: 1 }}
+        alt=""
+        className="image-3"
+      />
+    </div>
+  );
+};
 
 export default MainPageIntro;
