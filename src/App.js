656b6bbec6c9353a0a5f6c27f2cba6c13d26fa3d
lizzykerrigan <lizzykerrigan@outlook.com>
2019-12-19 16:12:44 +0000

    add Footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ArchiveNav from "./components/ArchiveNav.js";
 import PouringNow from "./pages/PouringNow";
 import { Router } from "@reach/router";
 import Home from "./pages/Home";
+import { ArchiveFooter } from "./components/ArchiveFooter";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Home path="/" />
           <PouringNow path="/pouringnow" />
         </Router>
+      <ArchiveFooter />
     </div>
   );
 }
---
+++ src/components/ArchiveFooter.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const ArchiveFooter = () => {
+  return (
+    <footer className="section-2">
+      <div className="footer-section">
+        <div className="footer-wrap">
+          <a
+            href="https://webflow.com/"
+            target="_blank"
+            className="webflow-link w-inline-block"
+          >
+            <div className="paragraph-tiny">Powered by Webflow</div>
+            <p className="paragraph-tiny">
+              Site by Lizzy Kerrigan
+              <br />© 2019
+            </p>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
