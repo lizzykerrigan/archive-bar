2270849c2347c3b81b298ddc77ebdc43c5e9898b
lizzykerrigan <lizzykerrigan@outlook.com>
2019-12-19 15:31:29 +0000

    new component for pouring introhead

---
+++ src/components/PouringNowIntro.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const PouringNowIntro = () => {
+  return (
+    <div class="section-2 cc-home-wrap">
+      <div class="intro-head cc-subpage">
+        <div class="intro-content">
+          <div class="heading-jumbo">
+            Pouring Now
+            <br />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PouringNowIntro;
