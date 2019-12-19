348bdfa5ded17f594c5f8ba8d5d1710a17672d87
lizzykerrigan <lizzykerrigan@outlook.com>
2019-12-19 15:30:23 +0000

    remove introhead

--- src/components/ArchiveNav.js
+++ src/components/ArchiveNav.js
@@ -34,24 +34,6 @@ export default class ArchiveNav extends Component {
           </div>
           <div className="w-nav-overlay" data-wf-ignore=""></div>
         </div>
-        <div className="section-2 cc-store-home-wrap">
-          <div className="intro-head">
-            <img
-              src="https://uploads-ssl.webflow.com/5df20ac39bca42668d1d0fac/5df2a3625526fc59befe5c3c_53169366_249289682676967_8442834466077933568_n%20(1).png"
-              width="480"
-              srcSet="
-          https://uploads-ssl.webflow.com/5df20ac39bca42668d1d0fac/5df2a3625526fc59befe5c3c_53169366_249289682676967_8442834466077933568_n%20(1)-p-500.png 500w,
-          https://uploads-ssl.webflow.com/5df20ac39bca42668d1d0fac/5df2a3625526fc59befe5c3c_53169366_249289682676967_8442834466077933568_n%20(1)-p-800.png 800w,
-          https://uploads-ssl.webflow.com/5df20ac39bca42668d1d0fac/5df2a3625526fc59befe5c3c_53169366_249289682676967_8442834466077933568_n%20(1).png       960w
-        "
-              sizes="100vw"
-              data-w-id="25adf555-9902-15fe-6052-25565d02d245"
-              style={{opacity: 1}}
-              alt=""
-              className="image-3"
-            />
-          </div>
-        </div>
       </>
     );
   }
