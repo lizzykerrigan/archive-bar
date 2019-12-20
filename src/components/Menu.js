5334e5d6712fe8d6372abbb06c1e9ebb57ce719a
Lizzy Kerrigan <lizzykerrigan@outlook.com>
2019-12-20 17:52:58 +0000

    add menu grid component to be renamed later

---
+++ src/components/Menu.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Menu.css";
+
+const Menu = () => {
+  return (
+    <div class="section group">
+      <div class="col span_1_of_2">
+        <span className="menu-heading">Drinks</span>
+      </div>
+      <div class="col span_2_of_2" />
+    </div>
+  );
+};
+
+export default Menu;
