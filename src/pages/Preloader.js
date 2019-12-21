91f24adf7fe884fad1dab2c19909a99bc1713927
Lizzy Kerrigan <lizzykerrigan@outlook.com>
2019-12-21 20:03:33 +0000

    add preloader elements

---
+++ src/pages/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Preloader = () => {
+  return (
+    <div class="preloader">
+      <img src="tailspin.svg" alt="spinner" />
+    </div>
+  );
+};
+
+export default Preloader;
