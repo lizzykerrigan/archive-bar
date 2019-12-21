13bf4f62e8a457cec18bf0de6fc261f26dd4acbd
Lizzy Kerrigan <lizzykerrigan@outlook.com>
2019-12-21 20:06:22 +0000

    add Gallery to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import MainPageDescription from "../components/MainPageDescription";
 import MainPageIntro from "../components/MainPageIntro";
+import Gallery from "../components/Gallery";
 
 const Home = () => {
   return (
     <>
       <MainPageIntro />
       <MainPageDescription />
+      <Gallery />
     </>
   );
 };
