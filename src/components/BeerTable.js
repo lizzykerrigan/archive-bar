2ac900e1bfd4d91a72ffe0f621a69cc2638c05e9
Lizzy Kerrigan <lizzykerrigan@outlook.com>
2019-12-22 12:55:15 +0000

    configure table with Contentful data

--- src/components/BeerTable.js
+++ src/components/BeerTable.js
@@ -1,118 +1,83 @@
-import React from "react";
+import React, { Component } from "react";
 import "./BeerTable.css";
 
-const BeerTable = () => {
-  return (
-    <div className="beer-table">
+import * as contentful from "contentful";
+
+import { REACT_APP_CTF_SPACE, REACT_APP_CTF_CDA_TOKEN } from "../env";
+
+class BeerTable extends Component {
+  state = {
+    fields: [],
+    headings: [],
+    loaded: false
+  };
+
+  client = contentful.createClient({
+    space: REACT_APP_CTF_SPACE,
+    accessToken: REACT_APP_CTF_CDA_TOKEN
+  });
+
+  componentDidMount() {
+    this.fetchPosts().then(this.setPosts);
+  }
+
+  fetchPosts = () => this.client.getEntries({ content_type: "pouring" });
+
+  setPosts = response => {
+    const fields = response.items.map(el => el.fields);
+    this.setState({
+      fields,
+      headings: Object.keys(fields[0]),
+      loaded: true
+    });
+  };
+
+  compareFieldsByLineNumber = (a, b) => {
+    const lineA = a.line.slice(-2);
+    const lineB = b.line.slice(-2);
+
+    return lineA - lineB;
+  };
+
+  compareFieldsByLineType = (a, b) => {
+    const lineA = a.line.slice(0, 4).toUpperCase();
+    const lineB = b.line.slice(0, 4).toUpperCase();
+    return lineB.localeCompare(lineA);
+  };
+
+  render() {
+    const { fields, headings, loaded } = this.state;
+    fields
+      .sort(this.compareFieldsByLineNumber)
+      .sort(this.compareFieldsByLineType);
+
+    return (
+      <div className="beer-table">
         <h1>Beer Board</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>IP</th>
-            <th>Server FQDN</th>
-            <th>Type</th>
-            <th>OS</th>
-            <th>Memory</th>
-            <th>CPU</th>
-            <th>Bind Type</th>
-            <th>Exim Type</th>
-            <th>Instance(AWS)</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>56.208.157.93</td>
-            <td>filler</td>
-            <td>AWS</td>
-            <td>Ubuntu</td>
-            <td>3840 MB</td>
-            <td>2.60 GHz</td>
-            <td>master</td>
-            <td>master</td>
-            <td>m3.medium</td>
-          </tr>
-          <tr>
-            <td>56.208.157.93</td>
-            <td>filler</td>
-            <td>AWS</td>
-            <td>Ubuntu</td>
-            <td>3840 MB</td>
-            <td>2.60 GHz</td>
-            <td>master</td>
-            <td>master</td>
-            <td>m3.medium</td>
-          </tr>
-          <tr>
-            <td>56.208.157.93</td>
-            <td>filler</td>
-            <td>AWS</td>
-            <td>Ubuntu</td>
-            <td>3840 MB</td>
-            <td>2.60 GHz</td>
-            <td>master</td>
-            <td>master</td>
-            <td>m3.medium</td>
-          </tr>
-          <tr>
-            <td>56.208.157.93</td>
-            <td>filler</td>
-            <td>AWS</td>
-            <td>Ubuntu</td>
-            <td>3840 MB</td>
-            <td>2.60 GHz</td>
-            <td>master</td>
-            <td>master</td>
-            <td>m3.medium</td>
-          </tr>
-          <tr>
-            <td>56.208.157.93</td>
-            <td>filler</td>
-            <td>AWS</td>
-            <td>Ubuntu</td>
-            <td>3840 MB</td>
-            <td>2.60 GHz</td>
-            <td>master</td>
-            <td>master</td>
-            <td>m3.medium</td>
-          </tr>
-          <tr>
-            <td>56.208.157.93</td>
-            <td>filler</td>
-            <td>AWS</td>
-            <td>Ubuntu</td>
-            <td>3840 MB</td>
-            <td>2.60 GHz</td>
-            <td>master</td>
-            <td>master</td>
-            <td>m3.medium</td>
-          </tr>
-          <tr>
-            <td>56.208.157.93</td>
-            <td>filler</td>
-            <td>AWS</td>
-            <td>Ubuntu</td>
-            <td>3840 MB</td>
-            <td>2.60 GHz</td>
-            <td>master</td>
-            <td>master</td>
-            <td>m3.medium</td>
-          </tr>
-          <tr>
-            <td>56.208.157.93</td>
-            <td>filler</td>
-            <td>AWS</td>
-            <td>Ubuntu</td>
-            <td>3840 MB</td>
-            <td>2.60 GHz</td>
-            <td>master</td>
-            <td>master</td>
-            <td>m3.medium</td>
-          </tr>
-          
-        </tbody>
-      </table>
-    </div>
-  );
-};
+        <table>
+          <thead>
+            <tr>
+              {loaded ? headings.map(heading => <th>{heading}</th>) : null}
+            </tr>
+          </thead>
+          <tbody>
+            {loaded
+              ? fields.map((el, index) => (
+                  <tr key={index}>
+                    <td>{el.line}</td>
+                    <td>{el.beer}</td>
+                    <td>{el.style}</td>
+                    <td>{el.brewery}</td>
+                    <td>{el.strength}</td>
+                    <td>{el.price}</td>
+                  </tr>
+                ))
+              : null}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 export default BeerTable;
