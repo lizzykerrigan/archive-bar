c468e72883e743d9e7765b0329093c10d657be5d
Lizzy Kerrigan <lizzykerrigan@outlook.com>
2019-12-22 17:43:50 +0000

    change footer text

--- src/components/ArchiveFooter.js
+++ src/components/ArchiveFooter.js
@@ -5,17 +5,10 @@ export const ArchiveFooter = () => {
     <footer className="section-2">
       <div className="footer-section">
         <div className="footer-wrap">
-          <a
-            href="https://webflow.com/"
-            target="_blank"
-            className="webflow-link w-inline-block"
-          >
-            <div className="paragraph-tiny">Powered by Webflow</div>
-            <p className="paragraph-tiny">
-              Site by Lizzy Kerrigan
-              <br />© 2019
-            </p>
-          </a>
+          <p className="paragraph-tiny">
+            Site by Lizzy Kerrigan
+            <br />© 2019
+          </p>
         </div>
       </div>
     </footer>
