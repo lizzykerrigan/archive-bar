9061941ea41e247008a01a8d90db661a60dc0068
Lizzy Kerrigan <lizzykerrigan@outlook.com>
2019-12-23 16:54:41 +0000

    add WinesList component

---
+++ src/components/WinesList.js
@@ -0,0 +1,114 @@
+import React, { Component } from "react";
+import "./DrinksList.css";
+
+import * as contentful from "contentful";
+
+import { REACT_APP_CTF_SPACE, REACT_APP_CTF_CDA_TOKEN } from "../env";
+
+class WinesList extends Component {
+  state = {
+    fields: [],
+    loaded: false
+  };
+
+  client = contentful.createClient({
+    space: REACT_APP_CTF_SPACE,
+    accessToken: REACT_APP_CTF_CDA_TOKEN
+  });
+
+  componentDidMount() {
+    this.fetchPosts().then(this.setPosts);
+  }
+
+  fetchPosts = () =>
+    this.client.getEntries({ content_type: this.props.listName });
+
+  filterWines = (fields, type) => {
+    return fields.filter(
+      entry => entry.type.toUpperCase() === type.toUpperCase()
+    );
+  };
+
+  setPosts = response => {
+    const fields = response.items.map(el => el.fields);
+    this.setState({
+      fields,
+      loaded: true
+    });
+  };
+
+  render() {
+    const { fields, loaded } = this.state;
+
+    fields.sort((a, b) => a.price.slice(-5) - b.price.slice(-5));
+
+    return (
+      <div className={`drinks-list wines-list`}>
+        <h2>Wines</h2>
+        <h3>Red</h3>
+        <ul>
+          {loaded
+            ? this.filterWines(fields, "red").map(el => (
+                <li>
+                  <strong>{el.name}</strong>
+                  <br />
+                  {el.price}
+                </li>
+              ))
+            : null}
+        </ul>
+        <h3>White</h3>
+        <ul>
+          {loaded
+            ? this.filterWines(fields, "white").map(el => (
+                <li>
+                  <strong>{el.name}</strong>
+                  <br />
+                  {el.price}
+                </li>
+              ))
+            : null}
+        </ul>
+        {this.filterWines(fields, "rose").length > 0 && (
+          <>
+            <h3>Rosé</h3>
+            <ul className="single-col">
+              {loaded
+                ? this.filterWines(fields, "rose").map(el => (
+                    <li>
+                      <strong>{el.name}</strong>
+                      <br />
+                      {el.price}
+                    </li>
+                  ))
+                : null}
+            </ul>
+          </>
+        )}
+        {this.filterWines(fields, "fizz").length > 0 && (
+          <>
+            <h3>Fizz</h3>
+            <ul className="single-col">
+              {loaded
+                ? this.filterWines(fields, "fizz").map(el => (
+                    <li>
+                      <strong>{el.name}</strong>
+                      <br />
+                      {el.price}
+                    </li>
+                  ))
+                : null}
+            </ul>
+          </>
+        )}
+        <img
+          src="divider-bottom.svg"
+          aria-hidden="true"
+          className="bottom-divider"
+        />
+      </div>
+    );
+  }
+}
+
+export default WinesList;
