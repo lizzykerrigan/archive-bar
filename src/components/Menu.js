0c11df9e33bf19981e30c5fce13e187a8dbd834e
Lizzy Kerrigan <lizzykerrigan@outlook.com>
2019-12-23 22:34:03 +0000

    change menu span to h1

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,7 +5,7 @@ const Menu = () => {
   return (
     <div class="section group">
       <div class="col span_1_of_2">
-        <span className="menu-heading">Drinks</span>
+        <h1 className="menu-heading">Drinks</h1>
       </div>
       <div class="col span_2_of_2">
         <img className="column-img" src="https://res.cloudinary.com/dpj3m4zwi/image/upload/f_auto,q_auto/v1576941701/76935991_415094802763120_6370016677035245568_n.jpg.jpg" />
