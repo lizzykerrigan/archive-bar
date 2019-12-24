ce50f186139d46a2fe6b0caa59c5709c491f9616
Lizzy Kerrigan <lizzykerrigan@outlook.com>
2019-12-24 16:35:55 +0000

    add Heading prop

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./Menu.css";
 
-const Menu = () => {
+const Menu = props => {
   return (
     <div class="section group">
-      <div class="col span_1_of_2">
-        <h1 className="menu-heading">Drinks</h1>
+      <div class={"col span_1_of_2"}>
+        <h1 className="menu-heading">{props.heading}</h1>
       </div>
       <div class="col span_2_of_2">
-        <img className="column-img" src="https://res.cloudinary.com/dpj3m4zwi/image/upload/f_auto,q_auto/v1576941701/76935991_415094802763120_6370016677035245568_n.jpg.jpg" />
+        <img
+          className="column-img"
+          src="https://res.cloudinary.com/dpj3m4zwi/image/upload/f_auto,q_auto/v1576941701/76935991_415094802763120_6370016677035245568_n.jpg.jpg"
+        />
       </div>
     </div>
   );
