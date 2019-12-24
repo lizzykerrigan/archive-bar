33d5bfd8b131aab2a2a6b28a97fb2be3772cf0c3
Lizzy Kerrigan <lizzykerrigan@outlook.com>
2019-12-24 18:39:37 +0000

    remove braces

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,7 +4,7 @@ import "./Menu.css";
 const Menu = props => {
   return (
     <div class="section group">
-      <div class={"col span_1_of_2"}>
+      <div class="col span_1_of_2">
         <h1 className="menu-heading">{props.heading}</h1>
       </div>
       <div class="col span_2_of_2">
