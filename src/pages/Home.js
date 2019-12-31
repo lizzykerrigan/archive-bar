a475012cab99e2a4a3fad911366c24e51305f7ed
Lizzy Kerrigan <lizzykerrigan@outlook.com>
2019-12-31 16:23:57 +0000

    add loading state and other pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MainPageDescription from "../components/MainPageDescription";
 import MainPageIntro from "../components/MainPageIntro";
 import Gallery from "../components/Gallery";
+import Drinks from "./Drinks";
+import Food from "./Food";
+import Preloader from "./Preloader";
+import { ContactPage } from "./ContactPage";
 
 const Home = () => {
-  document.title = "Archive Bar & Bottle, Cheadle Hulme"
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 2000);
+  }, []);
+
+  document.title = "Archive Bar & Bottle, Cheadle Hulme";
   return (
     <>
+      {!loaded && <Preloader />}
       <MainPageIntro />
       <MainPageDescription />
       <Gallery />
+      <Drinks />
+      <Food />
+      <ContactPage />
     </>
   );
 };
