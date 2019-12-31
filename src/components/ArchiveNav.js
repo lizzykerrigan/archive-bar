f34db962e3b57107a69701334c7b2d70b6abb9c8
Lizzy Kerrigan <lizzykerrigan@outlook.com>
2019-12-31 16:23:24 +0000

    add icons for mobile view (still need alt text)

--- src/components/ArchiveNav.js
+++ src/components/ArchiveNav.js
@@ -14,22 +14,29 @@ export default class ArchiveNav extends Component {
             <a href="/" className="logo-link w-nav-brand w--current">
               <img
                 src="https://res.cloudinary.com/dpj3m4zwi/image/upload/f_auto,q_auto/v1577184645/archive_header_logo.png"
-                width="107"
-                sizes="107px"
                 alt=""
                 className="logo-image"
               />
             </a>
             <div className="menu">
               <nav role="navigation" className="navigation-items w-nav-menu">
-                <a href="/drinks" className="navigation-item w-nav-link">
+                <a href="#drinks-menu" className="navigation-item w-nav-link hidden-responsive">
                   Drinks
                 </a>
-                <a href="/food" className="navigation-item w-nav-link">
+                <a href="#drinks-menu" className="navigation-item w-nav-link visible-responsive">
+                  <img src="wine.svg" aria-hidden />
+                </a>
+                <a href="#food-menu" className="navigation-item w-nav-link hidden-responsive">
                   Food
                 </a>
-                <a href="/contact" className="navigation-item w-nav-link">
+                <a href="#food-menu" className="navigation-item w-nav-link visible-responsive">
+                  <img src="restaurant.svg" aria-hidden />
+                </a>
+                <a href="#contact" className="navigation-item w-nav-link hidden-responsive">
                   Contact
+                </a>
+                <a href="#contact" className="navigation-item w-nav-link visible-responsive">
+                  <img src="chat-alt.svg" aria-hidden />
                 </a>
               </nav>
             </div>
