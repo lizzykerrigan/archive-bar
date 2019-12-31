26e860e311eccc891cf6fb551d5b0345d4de01c4
Lizzy Kerrigan <lizzykerrigan@outlook.com>
2019-12-31 18:35:14 +0000

    destructure props, change class to className

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./Menu.css";
 
-const Menu = props => {
+const Menu = ({heading, image}) => {
   return (
-    <div class="section group">
-      <div class="col span_1_of_2">
-        <h1 className="menu-heading">{props.heading}</h1>
+    <div className="section group">
+      <div className="col span_1_of_2">
+        <h1 className="menu-heading">{heading}</h1>
       </div>
-      <div class="col span_2_of_2">
+      <div className="col span_2_of_2">
         <img
           className="column-img"
-          src={props.image}
+          src={image}
         />
       </div>
     </div>
