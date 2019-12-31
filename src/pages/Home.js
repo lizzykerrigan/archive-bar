27792539ec1b41c4093b9f2bf706fb8068ed1174
Lizzy Kerrigan <lizzykerrigan@outlook.com>
2019-12-31 21:28:41 +0000

    comment out Gallery for now

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
       {!loaded && <Preloader />}
       <MainPageIntro />
       <MainPageDescription />
-      <Gallery />
+      {/* <Gallery /> */}
       <Drinks />
       <Food />
       <ContactPage />
