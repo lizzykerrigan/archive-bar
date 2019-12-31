e5955529b0dffbe78b513c7f7e98ea5790b8cfbc
Lizzy Kerrigan <lizzykerrigan@outlook.com>
2019-12-31 22:11:58 +0000

    add contact section image

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ContactComponent from "../components/ContactComponent";
+import Menu from "../components/Menu";
 
 export const ContactPage = () => {
   return (
     <section id="contact">
+      <Menu heading="contact" />
       <h1>CONTACT US</h1>
       <ContactComponent />
     </section>
