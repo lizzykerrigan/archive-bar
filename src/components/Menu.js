7b0379d8bce4851f3acf01bcb74c9af133c6297f
Lizzy Kerrigan <lizzykerrigan@outlook.com>
2019-12-31 21:27:53 +0000

    remove columns, add fluid container

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,17 +1,13 @@
 import React from "react";
 import "./Menu.css";
 
-const Menu = ({heading, image}) => {
+const Menu = ({ heading, image }) => {
   return (
     <div className="section group">
-      <div className="col span_1_of_2">
-        <h1 className="menu-heading">{heading}</h1>
-      </div>
-      <div className="col span_2_of_2">
-        <img
-          className="column-img"
-          src={image}
-        />
+      <div className="container-fluid">
+        <div className="bg-img">
+          <div className={`bg-img-${heading}`} />
+        </div>
       </div>
     </div>
   );
