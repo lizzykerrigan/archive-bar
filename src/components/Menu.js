a10bd6ea141a1a89a64af9c720bcaad97661d849
Lizzy Kerrigan <lizzykerrigan@outlook.com>
2019-12-31 16:24:59 +0000

    get image from props on Menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,7 +10,7 @@ const Menu = props => {
       <div class="col span_2_of_2">
         <img
           className="column-img"
-          src="https://res.cloudinary.com/dpj3m4zwi/image/upload/f_auto,q_auto/v1576941701/76935991_415094802763120_6370016677035245568_n.jpg.jpg"
+          src={props.image}
         />
       </div>
     </div>
