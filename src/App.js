278ce474e584bdf76d4681118af796c7878e2c46
Lizzy Kerrigan <lizzykerrigan@outlook.com>
2019-12-31 16:23:06 +0000

    remove router from entry

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from "react";
 import ArchiveNav from "./components/ArchiveNav.js";
-import Drinks from "./pages/Drinks";
-import Food from "./pages/Food";
-import { Router } from "@reach/router";
 import Home from "./pages/Home";
 import { ArchiveFooter } from "./components/ArchiveFooter";
-import { ContactPage } from "./pages/ContactPage";
 
 function App() {
   return (
     <div className="App">
       <ArchiveNav />
-        <Router>
-          <Home path="/" />
-          <Drinks path="/drinks" />
-          <Food path="/food" />
-          <ContactPage path="/contact" />
-        </Router>
+      <Home />
       <ArchiveFooter />
     </div>
   );
