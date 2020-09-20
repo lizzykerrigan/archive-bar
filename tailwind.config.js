60e74354a61e4d4ba2039fc38671e15eec8027ed
lizzykerrigan <lizzykerrigan@outlook.com>
2020-09-20 20:11:24 +0100

    add purge to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: ['./src/components/**/*.tsx', './src/pages/**/*.tsx'],
+  },
   target: 'relaxed',
   prefix: '',
   important: false,
