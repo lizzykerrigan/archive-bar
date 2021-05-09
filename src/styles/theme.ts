1038406949847b86b6208bc675701702d9662724
lizzykerrigan <lizzykerrigan@outlook.com>
2021-05-09 09:13:19 +0100

    add theme object

---
+++ src/styles/theme.ts
@@ -0,0 +1,7 @@
+export const theme = {
+  background: '#fefefe',
+  color: '#1a1b1f',
+  archiveGreen: '#687037',
+  fontFamily:
+    'EB Garamond, -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+};
