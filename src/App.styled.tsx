42660aae23ba7cdd3a8c95ea1876377f04092765
lizzykerrigan <lizzykerrigan@outlook.com>
2021-05-09 09:13:11 +0100

    update App: include router and base styles

---
+++ src/App.styled.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { theme } from './styles/theme';
+
+export const App = styled.div`
+  background: ${theme.background};
+  color: ${theme.color};
+  font-family: ${theme.fontFamily};
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import ArchiveNav from './components/ArchiveNav';
-import Home from './pages/Home';
+// import ArchiveNav from './components/ArchiveNav';
 import ArchiveFooter from './components/ArchiveFooter/';
+import Header from './components/Header';
+import Home from './pages/Home';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { App as StyledApp } from './App.styled';
 
 function App() {
   return (
-    <div className="App">
-      <ArchiveNav />
-      <Home />
-      <ArchiveFooter />
-    </div>
+    <StyledApp>
+      <Router>
+        <Header />
+        <Home />
+        <ArchiveFooter />
+      </Router>
+    </StyledApp>
   );
 }
 
